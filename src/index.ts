63d4e4d2709fb2683cc307cbdd454656931a6732
Michael Seaward <m.seaward@pm.me>
2024-04-17 16:46:00 -0300

    feat: add jwt support and test protected route

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import cors from 'cors';
-import express, { Application, Request, Response } from 'express';
+import express, { Application, NextFunction, Request, Response } from 'express';
+import { verify } from 'jsonwebtoken';
 import routes from './api/routers';
 import dbInit from './db/init';
+import User from './db/models/user';
 
 dbInit();
 
+// set port, listen for requests
+const port = process.env.PORT || 8080;
+const jwtToken = process.env.JWT_TOKEN;
 const app: Application = express();
 
 app.use(
@@ -29,12 +34,48 @@ app.get('/test', async (req: Request, res: Response): Promise<Response> => {
   return res.status(200).send('Well done! This is the test endpoint.');
 });
 
-// set port, listen for requests
-const PORT = process.env.PORT || 8080;
+app.get(
+  '/protected-test',
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const token = req.headers['authorization']?.split(' ')[1] || '';
+
+      console.log('token', token);
+      if (!token) {
+        return res.status(403).send('A token is required for authentication');
+      }
+
+      const decodedToken = verify(token, jwtToken || 'dumb_secret');
+
+      // @ts-expect-error
+      req.user = decodedToken;
+
+      return next();
+    } catch (err) {
+      console.error(err);
+      return res.status(401).json({ msg: 'Couldnt Authenticate' });
+    }
+  },
+  async (req: Request, res: Response) => {
+    const user = await User.findOne({
+      where: {
+        // @ts-expect-error
+        id: req.user.id,
+      },
+      attributes: { exclude: ['password'] },
+    });
+
+    if (user === null) {
+      return res.status(404).json({ msg: 'User not found' });
+    }
+
+    return res.status(200).json(user);
+  }
+);
 
 try {
-  app.listen(PORT, () => {
-    console.log(`The application is listening on port ${PORT}!`);
+  app.listen(port, () => {
+    console.log(`The application is listening on port ${port}!`);
   });
 } catch (error) {
   console.error(error);
